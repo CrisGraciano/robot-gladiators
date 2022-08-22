f6975a472ed29fe0e90b69ba15789f1287bf41e2
CrisGraciano <cristobalgraciano98@gmail.com>
2022-08-21 20:53:59 -0500

    pre mvp

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-var playerName = window.prompt ("What is your robot's name?");
+var playerName = window.prompt ("What is your robot's name?")
 var playerHealth = 100;
 var playerAttack = 10;
 
@@ -13,17 +13,39 @@ var fight = function () {
     window.alert("Welcome to Robot Gladiators!");
 
     // Subtract the value of 'playerAttack from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
-    enemyHealth = enemyHealth - playerAttack
+    enemyHealth = enemyHealth - playerAttack;
 
     // Log a resulting message to the console so we know it worked.
-    playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining."
+    console.log(
+         playerName + "attacked " + enemyName + ". " + enemyName + "now has " + enemyHealth + " health remaining."
+    );
+
+    // Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + "has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
 
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth variable.
-    
+    playerHealth = playerHealth - enemyAttack
+
     // Log a resulting message to the console so we know it worked.
+    console.log(
+        enemyName + "attacked" + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
+    );
+
+    // Check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 
     
 
 };
 
-fight ()
+fight ();
